481749cea0b4ddf516ef748d760a3d74882250dd
ALex-Hwang <3180102081@zju.edu.cn>
2020-11-27 21:46:45 +0800

    sdlkfj

---
+++ work/test.c
@@ -0,0 +1,11 @@
+#include<unistd.h>
+#include<sys/syscall.h>
+#include<stdio.h>
+#include<stdlib.h>
+#define __NR_mysyscall 223
+
+int main()
+{
+        syscall(__NR_mysyscall);
+        return 0;
+}
